9cf74bee36684b415ac495380d50470ca2040329
Purva Udai <purvaudai@localhost.localdomain>
2019-06-17 23:48:25 +0530

    Created users and enabled login and sign up button

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boards',
+    'accounts',
     'widget_tweaks',
 ]
 
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,10 +18,14 @@ from django.urls import path
 
 
 from boards import views
+from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout')
 ]
